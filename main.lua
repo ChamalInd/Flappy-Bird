137c8188eeb55d4a4c6cbfaf1855fb9cbed8b50d
Chamal Induwara <88148370+ChamalInd@users.noreply.github.com>
2026-04-29 22:35:34 +0530

    Added pipes

--- main.lua
+++ main.lua
@@ -14,17 +14,25 @@ local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
 
+-- variables for spawning pipes
+local pipes = {}
+local spawnTimer = 0
+
 -- importing external libraries
 push = require('Libraries/push')
 Class = require('Libraries/class')
 
 -- importing objects
 require('Objects/Bird')
+require('Objects/Pipe')
 
 
 function love.load()
     -- adding filter for a pixelated view 
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- seeding random function 
+    math.randomseed(os.time())
 
     -- setting images 
     background = love.graphics.newImage('Images/background.png')
@@ -80,7 +88,25 @@ function love.update(dt)
     backgroundScroll = (backgroundScroll + (BACKGROUND_SCROLL_SPEED * dt)) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + (GROUND_SCROLL_SPEED * dt)) % VIRTUAL_WIDTH
 
+    -- adding pipes every 2 second
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer > 2 then
+        table.insert(pipes, Pipe())
+        spawnTimer = 0
+    end
+
     bird:update(dt)
+
+    -- moving pipes 
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+
+        -- remove unnecessory pipes 
+        if pipe.x < -pipe.width then
+            table.remove(pipe, k)
+        end
+    end
 
     -- resetting keyboard memory
     love.keyboard.keysPressed = {}
@@ -92,10 +118,16 @@ function love.draw()
 
     -- drawing backgrounds 
     love.graphics.draw(background, -backgroundScroll, 0)
-    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)  
     
     -- drawing bird 
     bird:render()
+
+    -- drawing pipes 
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
+
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)  
 
     push:finish()
 end
